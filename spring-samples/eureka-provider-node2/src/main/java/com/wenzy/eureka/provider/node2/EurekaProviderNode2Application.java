d92e3243730dd3ca07c754fa30993d9488c81324
文智勇 <411693881@qq.com>
2021-03-01 14:01:46 +0800

    Add files via upload

---
+++ spring-samples/eureka-provider-node2/src/main/java/com/wenzy/eureka/provider/node2/EurekaProviderNode2Application.java
@@ -0,0 +1,14 @@
+package com.wenzy.eureka.provider.node2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class EurekaProviderNode2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaProviderNode2Application.class, args);
+	}
+}
